1aa06f7f42c6cad4f6d4154387de2fb01acc6109
RaoulBoulos <raoul.boulos@net.usj.edu.lb>
2021-05-18 06:23:46 +0300

    Model Change

--- models/restaurantReview.js
+++ models/restaurantReview.js
@@ -2,6 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const restaurantReviewSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
     stars: {
         type: String,
         required: true
